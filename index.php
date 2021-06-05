39fbe6ec71404dce3a8620a79c6cacd7967b48aa
Gabriel Azevedo <azevgabriel@gmail.com>
2021-06-05 12:37:33 -0300

    Delete Bootstrap, new Global & Home Styles

--- index.php
+++ index.php
@@ -4,14 +4,34 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tassi Transporte</title>
+  <title>Student Controller</title>
+  <link rel="stylesheet" href="src/styles/global.css">
+  <link rel="stylesheet" href="src/styles/pages/home.css">
 </head>
 <body>
-  <?php
-    // Página Inicial
-    require_once 'src/controller/classesController.php'; // Carrega o arquivo classesController.php
-    $classesController = new classesController(); // classesController
-    $classesController -> index(); // Chama o método index() do controlador
-  ?>
+  <div class="container">
+    <nav>
+      <img src="public/img/logo.svg"/>
+      <h1>Controle de Estudantes</h1>
+      <div class="nav">
+        <button>Cadastar</button>
+        <br />
+        <button>Listar</button>
+      </div>
+      <p>
+        Crud básico feito por 
+        <a href="https://github.com/azevgabriel">Gabriel Azevedo</a>
+        ,como objetivo de desenvolver um sistema de controle de alunos de uma escola.
+      </p>
+    </nav>
+    
+    <article>
+    <?php
+      require_once 'src/controller/classesController.php'; // Carrega o arquivo classesController.php
+      $classesController = new classesController(); // classesController
+      $classesController -> index(); // Chama o método index() do controlador
+    ?>
+    </article>
+  <div>
 </body>
 </html>
--- src/lib/connection.php
+++ src/lib/connection.php
@@ -1,8 +1,10 @@
 <?php
+    // Classe responsavél por realizar a conexão.
     class Connection
     {
         public function getConnect(){
             $conn = mysqli_connect("localhost:3306", "root", "", "adm_tassi");
+            //Verifica se a conexão falhou.
             if (!$conn) {
                 return ("A conexão com o Banco de Dados falhou!");
             }
